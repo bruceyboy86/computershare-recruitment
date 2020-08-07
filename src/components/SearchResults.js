2415732de4776f4749d240ddaa610e03d354f036
azubdavidson <bruce.davidson@nucleusfinancial.com>
2020-08-07 20:51:26 +0100

    added date picker. Fetches data using hooks

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -2,19 +2,24 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import * as Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const SearchResults = () => {
   const [data, setData] = useState({});
   const [query, setQuery] = useState("AMZN");
+  const [startDate, setStartDate] = useState(new Date());
 
   useEffect(() => {
     let ignore = false;
-    const today = Math.round((new Date()).getTime() / 1000);
-    const lastWeek = Math.round((new Date()).getTime() / 1000 - 600000);
-
+    // const today = Math.round((new Date()).getTime() / 1000);
+    // const lastWeek = Math.round((startDate).getTime() / 1000)-600000;
+    const queryDate = Math.round(new Date(startDate).getTime() / 1000)
+    const lastWeek = queryDate - 600000;
+    // console.log(Math.round(new Date(startDate).getTime() / 1000))
     async function fetchData() {
       const result = await axios(
-        "https://finnhub.io/api/v1/stock/candle?symbol="+ query +"&resolution=D&from="+lastWeek+"&to="+today+"&token=bsko48frh5rfr4cc6t20"
+        "https://finnhub.io/api/v1/stock/candle?symbol="+ query +"&resolution=D&from="+lastWeek+"&to="+queryDate+"&token=bsko48frh5rfr4cc6t20"
       );
       if (!ignore) setData(result.data);
     }
@@ -23,7 +28,8 @@ const SearchResults = () => {
     return () => {
       ignore = true;
     };
-  }, [query]);
+  }, [query, startDate]);
+  // console.log(data)
   // console.log(data.t ? data.t.map((c,index) => c.keys()) : null)
   // console.log(data.t ? data.t.map((t,index) => t.find(e => e)): null)
   // console.log(data ? data :null)
@@ -37,7 +43,7 @@ const SearchResults = () => {
         ))}
         {data.t.map((item,index) => (
           <li key={index.toString()}>
-            {new Date(item * 1000).toLocaleString()}
+            {new Date(item * 1000).toLocaleDateString('UTC', { year: 'numeric', month: 'long', day: 'numeric'})}
             {/* {new Date(item * 1000).toLocaleDateString('UTC', {year: 'numeric', month: 'numeric', day: 'numeric'} )} */}
           </li>
         ))}
@@ -87,9 +93,15 @@ const SearchResults = () => {
     }
   }
 
+
+
+
+// console.log(Date.parse(startDate).getTime()/1000 )
+// console.log('startDate :', Date.parse(startDate), new Date(startDate) )
   return (
     <>
-      <input value={query} onChange={e => setQuery(e.target.value.toUpperCase())} />
+        <DatePicker selected={startDate} value={startDate} onChange={date => setStartDate(date)} />
+        <input value={query} onChange={e => setQuery(e.target.value.toUpperCase())} />
         {dataMap()}
         <HighStockPrice d={data.c} />
         <LowStockPrice d={data.l} />
