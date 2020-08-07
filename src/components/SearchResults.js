4af65387806090e42b33b3d1af8d316c8fd981d3
azubdavidson <bruce.davidson@nucleusfinancial.com>
2020-08-07 19:22:25 +0100

    High stock price and low stock price

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import * as Highcharts from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
 
 const SearchResults = () => {
   const [data, setData] = useState({});
@@ -12,7 +14,7 @@ const SearchResults = () => {
 
     async function fetchData() {
       const result = await axios(
-        "https://finnhub.io/api/v1/stock/candle?symbol="+ query +"&resolution=1&from="+lastWeek+"&to="+today+"&token=bsko48frh5rfr4cc6t20"
+        "https://finnhub.io/api/v1/stock/candle?symbol="+ query +"&resolution=D&from="+lastWeek+"&to="+today+"&token=bsko48frh5rfr4cc6t20"
       );
       if (!ignore) setData(result.data);
     }
@@ -46,10 +48,52 @@ const SearchResults = () => {
     }
   }
 
+  const options = {
+    title: {
+      text: 'My stock chart'
+    },
+    // series: [{
+    //   data: [1, 2, 3]
+    // }],
+    series: [
+      // {data: [data.c]},
+      // {data: [data.h]},
+      // {data: data.l},
+      // {data: data.o},
+      // {data: data.v},
+      {
+        type: 'candlestick',
+        name: 'AAPL',
+        data: [data.t,data.o,data.h,data.l,data.c],
+    },
+    ]
+  }
+
+  const MyStockChart = () => <HighchartsReact
+    highcharts={Highcharts}
+    constructorType={'stockChart'}
+    options={options}
+  />
+
+  const HighStockPrice = (data) => {
+    if(data){
+      return <><span>High stock price {data.d ? Math.max.apply(null, data.d) : '...'}</span></>
+    }
+  }
+
+  const LowStockPrice = (data) => {
+    if(data){
+      return <><span>Low stock price {data.d ? Math.min.apply(null, data.d) : '...'}</span></>
+    }
+  }
+
   return (
     <>
       <input value={query} onChange={e => setQuery(e.target.value.toUpperCase())} />
         {dataMap()}
+        <HighStockPrice d={data.c} />
+        <LowStockPrice d={data.l} />
+        <MyStockChart />
     </>
   );
 }
